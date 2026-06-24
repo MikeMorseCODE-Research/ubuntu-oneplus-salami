c5275d5e3fd90abbe6c1a52164b73965d17ab960
MikeMorseCODE <anonymouswanderlust25@gmail.com>
2026-06-24 07:16:37 -0400

    Fix salami block ordering - must run before rm -rf linux

--- aston-kernel_build.sh
+++ aston-kernel_build.sh
@@ -31,11 +31,11 @@ $1/mkbootimg --header_version 4 --base 0x0 --os_version 15.0.0 --os_patch_level 
 rm $1/linux-oneplus-aston/usr/dummy
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=$1/linux-oneplus-aston/usr modules_install
 rm $1/linux-oneplus-aston/usr/lib/modules/**/build
-cd $1
-rm -rf linux
-
-dpkg-deb --build --root-owner-group linux-oneplus-aston
 
 cat $1/linux/arch/arm64/boot/Image $1/linux/arch/arm64/boot/dts/qcom/sm8550-oneplus-salami.dtb > $1/linux/Image_w_dtb
 gzip -f $1/linux/Image_w_dtb
 $1/mkbootimg --header_version 4 --base 0x0 --os_version 15.0.0 --os_patch_level 2025-02 --kernel $1/linux/Image_w_dtb.gz -o $1/boot_salami_8G.img
+cd $1
+rm -rf linux
+
+dpkg-deb --build --root-owner-group linux-oneplus-aston
